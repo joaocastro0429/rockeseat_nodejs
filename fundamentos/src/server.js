ca1bf9b15d2e15f04d21d3997349b50d96e0498f
joaocastro0429125 <joaocastro0429125@gmail.com>
2024-05-23 22:56:31 -0300

    add funcionalidade no POST

--- fundamentos/src/server.js
+++ fundamentos/src/server.js
@@ -1,12 +1,19 @@
 import http from 'node:http'
-
+const ussers=[]
 const server=http.createServer((req,res)=>{
  const {method,url}=req
 
     if(method==='GET'&& url==='/users'){
-        return res.end("Listagem de usuário")
+        return res
+        .setHeader('Content-type','application/json')
+        .end(JSON.stringify(ussers))
     }
     if(method==='POST'&& url==='/users'){
+        ussers.push({
+            id:1,
+            name:"joao",
+            email:"joaocastro@gmail.com"
+        })
         return res.end("Criacao de usuário")
     }
     console.log(method,url)
