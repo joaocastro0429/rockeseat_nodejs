02a98a2e396ec0b7d5f55354590d73d9e3b6caa1
joaocastro0429125 <joaocastro0429125@gmail.com>
2024-05-23 22:42:30 -0300

    criando rota

--- fundamentos/src/server.js
+++ fundamentos/src/server.js
@@ -1,6 +1,15 @@
 import http from 'node:http'
 
 const server=http.createServer((req,res)=>{
+ const {method,url}=req
+
+    if(method==='GET'&& url==='/users'){
+        return res.end("Listagem de usuário")
+    }
+    if(method==='POST'&& url==='/users'){
+        return res.end("Criacao de usuário")
+    }
+    console.log(method,url)
    
     return res.end('Hello world')
 })
